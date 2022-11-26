88bc944204ac92c088ceb6b40c38156d0e60bb36
Ronald Saenz <rjsaenz@espol.edu.ec>
2022-11-25 23:19:00 -0500

    Flask implementation with gunicorn

--- app/main.py
+++ app/main.py
@@ -53,6 +53,7 @@ from flask_cors import CORS, cross_origin
 # Your API definition
 app = Flask(__name__)
 CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 #####################################################################################################
 # Load Files and models
